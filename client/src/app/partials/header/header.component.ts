bd4550db6757bc7d11c656ee3580b3f077426097
sakshi bharti <sakshibharti1@gmail.com>
2019-03-28 17:22:52 -0400

    Secured Express API

--- client/src/app/partials/header/header.component.ts
+++ client/src/app/partials/header/header.component.ts
@@ -21,7 +21,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.user = new User();
-    this.user = JSON.parse(localStorage.getItem('user'));
+    
   }
 
   onLogoutClick(): void {
@@ -32,7 +32,11 @@ export class HeaderComponent implements OnInit {
   }
 
   isLoggedIn(): boolean {
-    return this.authService.loggedIn();
+    const result = this.authService.loggedIn();
+    if(result) {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    }
+    return result;
+  }
   }
 
-}
--- server/config/app.js
+++ server/config/app.js
@@ -97,7 +97,7 @@ passport.use(strategy);
 
 
 app.use('/api', indexRouter);
-app.use('/api/contact-list', contactRouter);
+app.use('/api/contact-list', passport.authenticate('jwt', {session: false}), contactRouter); 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname,'../../public/index.html'));
 })
